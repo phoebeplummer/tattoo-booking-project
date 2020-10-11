e802b5854c06792787f583bead788e1752cd651d
phoebeplummer <71644749+phoebeplummer@users.noreply.github.com>
2020-10-11 17:16:02 +0100

    Update sqlmodules.sql

--- sqlmodules.sql
+++ sqlmodules.sql
@@ -1,1 +1,3 @@
-
+bookingRequest = """
+INSERT INTO tblClients (firstName, lastName, email, phoneNumber, artistNumber, description, sizeWidth, sizeLength, month)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?) """
